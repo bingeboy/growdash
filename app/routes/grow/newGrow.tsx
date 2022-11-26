774a3088aab4af73d9fa2791a355f42fdb539f9f
JP McGarrity <design@jpmcgarrity.com>
2022-11-25 19:52:05 -0800

    added form and have it rendering ... need to add validation still

--- app/routes/grow/newGrow.tsx
+++ app/routes/grow/newGrow.tsx
@@ -1,36 +1,54 @@
 //New Grow Route
-import type { ActionFunction } from "@remix-run/node";
+import { ActionFunction, redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
 
-export const action: ActionFunction = () => {
+export const action: ActionFunction = async({ request }) => {
+    const form  = await request.formData();
+
+    //date used from form for record
+    let title = form.get("title");
+    let description = form.get("description");
+    let startDate = form.get("startDate");
+    let endDate = form.get("endDate");
+    let strain = form.get("strain");
+
     const grow = await db.grow.create({
         data: { 
             title,
-            body, //change to description 
+            description,
             startDate,
             endDate,
-            //not added yet
             strain
          },
     });
+
+    return redirect(`/grow/${grow.id}`);
 }
 
 export default  function NewGrowRoute() {
     return (
-        <div>
+        <form method="post">
             <label>Grow Name</label>
-            <input type="text" name="growTitle" />
+            <input type="text" name="title" />
         
+            <label>Expected Days</label>
+            <input type="number"></input>
+
             <label >Start Date</label>
-            <input type="text"  />
+            <input type="date"  />
         
             <label>End Date</label>
-            <input type="text" name="endDate"/>
+            <input type="date" name="endDate"/>
         
             <label>Strain</label>
             <input type="text" name="strain" />
         
             <label>grow Description</label>
             <input type="text" name="growDescription" />
-        </div>
+
+            <button type="submit" className="button">
+                Submit
+            </button>
+        </form>
     );
 }
